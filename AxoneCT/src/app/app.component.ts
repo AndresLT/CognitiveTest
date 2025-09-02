a1d952237dd8e0fd279357b484850d9366b12f38
Andres Lozano <ALozano14@slb.com>
2025-09-02 18:48:31 -0500

    Add Toast and Spinner

--- AxoneCT/src/app/app.component.ts
+++ AxoneCT/src/app/app.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SupabaseService } from './services/supabase.service';
+import { NgxSpinnerComponent, NgxSpinnerService } from "ngx-spinner";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'AxoneCT';
 
-  constructor(private readonly supabaseService: SupabaseService){
+  constructor(private readonly supabaseService: SupabaseService, private spinner: NgxSpinnerService, private toastr: ToastrService ){
 
   }
   
   async ngOnInit(): Promise<void> {
+    this.spinner.show();
     await this.loadItems()
     await this.loadItem()
-     
+    this.spinner.hide()
+    this.showSuccess()
   }
 
   
@@ -30,5 +34,9 @@ export class AppComponent implements OnInit {
   async loadItem() {
     const { data, error } = await this.supabaseService.getItemById('Users',1);
     if (!error) console.log(data);
+  }
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
   }
 }
