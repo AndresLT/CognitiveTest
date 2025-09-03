eabcbb3e793e2b25c8e4827e3a8e8b7ff6acec09
Andres Lozano <ALozano14@slb.com>
2025-09-03 01:10:25 -0500

    Welcome component and update logos

--- AxoneCT/src/app/app.routes.ts
+++ AxoneCT/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/welcome/welcome.component').then(m => m.WelcomeComponent)
+    }
+];
